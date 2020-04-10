cbee62a36dda050da962fa80d9c62bb9be5d1d28
takuyakasagawa <kasa.kasa.0062362@gmail.com>
2020-04-10 09:11:24 +0900

    edited

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import _ from 'lodash';
 
+let arr = [];
+
 function addRandomNum() {
-  let p = document.createElement('p')
-  p.innerHTML = "Random Number: " + _.random(0, 10).toString();
+  let p = document.createElement('p');
+  const randomNum = _.random(0, 10);
+  p.innerHTML = "Random Number: " + randomNum.toString();
   document.body.appendChild(p)
 
   // 「配列の箱の中の値が5つになったら、配列の箱の中にこれ以上値を増やさない」
   // 「配列の箱の中の値が5つ未満であれば、配列の値を後ろにどんどん追加していく」
-  //let arr = [];
-  //for (let i = 0; i < 4; i++) {
-  //  console.log(arr[i]);
-  //}
+  if (arr.length == 5) {
+    arr.push(randomNum);
+    arr.shift();
+    console.log(arr);
+  } else {
+    arr.push(randomNum);
+    console.log(arr);
+  }
 }
 
 {
